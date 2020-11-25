0c9f8da7918270f08b6bb92175450794b3ed47aa
Goblin011 <44930310+someone0010@users.noreply.github.com>
2020-11-25 09:39:13 +0100

    Update limitbypass.js

--- brainly/limitbypass.js
+++ brainly/limitbypass.js
@@ -2,5 +2,20 @@
  * Brainly homework answers view limit bypass by Naveq (https://github.com/Naveq)
  * @author Naveq
  */
-localStorage.clear();
+
+try {
+    // This one is a good first attempt 
+    localStorage.clear();
+} catch(e) {
+    // means that the webpage already added a protective proxy on localStorage
+    function getAlternateLocalStorage() {
+        var ifrm = document.createElement("iframe");
+        ifrm.setAttribute("src", "https://brainly.pl");
+        document.body.appendChild(ifrm);
+        return ifrm;
+    }
+    var i = getAlternateLocalStorage();
+    i.contentWindow.localStorage.clear();
+    document.body.removeChild(i);
+}
 document.getElementsByClassName('js-page-wrapper')[0].click();
